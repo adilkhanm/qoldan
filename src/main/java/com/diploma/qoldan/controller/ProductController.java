2082c2d405cb79930079dcfe30833dbcef8cc1dd
adilkhanm <adilkhan.muratov@gmail.com>
2023-04-20 15:59:16 +0600

    feat: add category & product controllers

--- src/main/java/com/diploma/qoldan/controller/ProductController.java
+++ src/main/java/com/diploma/qoldan/controller/ProductController.java
@@ -1,11 +1,12 @@
 package com.diploma.qoldan.controller;
 
+import com.diploma.qoldan.dto.ProductDto;
 import com.diploma.qoldan.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/product")
@@ -14,7 +15,41 @@ public class ProductController {
 
     private final ProductService service;
 
-//    @GetMapping
-//    public ResponseEntity<>
+    @GetMapping
+    public ResponseEntity<List<ProductDto>> getProducts() {
+        List<ProductDto> productDtoList = service.getProducts();
+        return ResponseEntity.ok(productDtoList);
+    }
+
+    @GetMapping("/user/{email}")
+    public ResponseEntity<List<ProductDto>> getProductsByUser(@PathVariable("email") String userEmail) {
+        List<ProductDto> productDtoList = service.getProductsByUser(userEmail);
+        return ResponseEntity.ok(productDtoList);
+    }
+
+    @GetMapping("/category/{id}")
+    public ResponseEntity<List<ProductDto>> getProductsByCategory(@PathVariable("id") Long categoryId) {
+        List<ProductDto> productDtoList = service.getProductsByCategory(categoryId);
+        return ResponseEntity.ok(productDtoList);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createProduct(@RequestBody ProductDto productDto) {
+        service.createProduct(productDto);
+        return ResponseEntity.ok("Product was successfully created");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> editProduct(@PathVariable("id") Long productId, @RequestBody ProductDto productDto) {
+        productDto.setId(productId);
+        service.editProduct(productDto);
+        return ResponseEntity.ok("Product was successfully updated");
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable("id") Long productId) {
+        service.deleteProductById(productId);
+        return ResponseEntity.ok("Product was successfully deleted");
+    }
 
 }
